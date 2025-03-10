e41f09429e66727956d967d7f12c91d8b500c496
wolf <ahmedragabyasin2020@gmail.com>
2025-03-10 23:20:42 +0200

    front

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 class BrandSeeder extends Seeder
 {
     /**
@@ -12,6 +14,28 @@ class BrandSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Brand::truncate();
+        $brands = [
+            'Adidas',
+            'Balmain',
+            'Balenciaga',
+            'Burberry',
+            'Kenzo',
+            'Givenchy',
+            'Zara',
+        ];
+
+        foreach ($brands as $brand) {
+            DB::table('brands')->insert([
+                'name' => $brand,
+                'slug' => Str::slug($brand),
+                'image' => null,
+                'is_active' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             // LaratrustSeeder::class,
             UserSeeder::class,
-            // UnitSeeder::class,
+            BrandSeeder::class,
 
         ]);
     }
