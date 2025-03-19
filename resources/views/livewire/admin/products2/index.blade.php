75c9a4e60394672166c37b711b24f7237b315d56
wolf <ahmedragabyasin2020@gmail.com>
2025-03-20 00:49:58 +0200

    front

--- resources/views/livewire/admin/products2/index.blade.php
+++ resources/views/livewire/admin/products2/index.blade.php
@@ -111,31 +111,7 @@
                                                     <button type="button" class="btn btn-sm btn-warning" wire:click='show({{ $product->id }})'>
                                                         <i class="fa fa-eye"></i>
                                                     </button>
-                                                    <button type="button" class="btn btn-dark btn-sm " data-toggle="modal"
-                                                            data-target="#showbarcode{{ $product->id }}" title="@lang('products.barcode')">
-                                                        <i class="fa fa-vcard"></i>
-                                                    </button>
-                                                    <div class="modal fade" id="showbarcode{{ $product->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                        <div class="modal-dialog">
-                                                            <div class="modal-content">
-                                                                <div class="modal-header">
-                                                                    <h5 class="modal-title" id="exampleModalLabel">{{ $product->product_name }}</h5>
-                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                        <span aria-hidden="true">&times;</span>
-                                                                    </button>
-                                                                </div>
-                                                                <div class="modal-body">
-                                                                    {{ $product->product_name }}
-                                                                    {!! DNS1D::getBarcodeHTML($product->code . '', 'C128') !!}
-                                                                    {{ $product->created_at->format('Y-m-d') }}
-                                                                </div>
-                                                                <div class="modal-footer">
-                                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">إلغاء</button>
-                                                                    {{-- <button type="submit" class="btn btn-primary">نعم</button> --}}
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
+                                                    @include('livewire.admin.products2.barcode')
                                                     <x-delete-modal :item="$product" />
                                                 </td>
                                             </tr>
